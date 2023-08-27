4f32c2b85f4f0485e9f8b87d305aaa2db0d5e3d5
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2023-08-28 00:36:34 +0200

    Initial commit

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ def main():
 		thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 		# dilate the thresholded image to fill in holes, then find contours
 		# on thresholded image
+		all_zeros = not np.any(thresh)
+		print(all_zeros)
 		thresh = cv2.dilate(thresh, None, iterations=2)
 		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_SIMPLE)
